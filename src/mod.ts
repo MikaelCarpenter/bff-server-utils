000f1723a0e18ee12dfca31c8d6cf01a8b2fd179
Mikael Carpenter <mikaelcarp.dev@gmail.com>
2024-05-21 22:36:31 -0700

    Add documentation for prove re-export

--- src/mod.ts
+++ src/mod.ts
@@ -6,6 +6,12 @@
  * Contains simple utilities I find useful when writing BFF servers.
  */
 
+/**
+ * Re-exporting the '@mikael/prove' package.
+ * This provides all the exports from '@mikael/prove' as a single 'prove' object.
+ *
+ * @see {@link https://jsr.io/@mikael/prove|@mikael/prove on jsr} for more information about the original package.
+ */
 export * as prove from '@mikael/prove';
 
 export { default as validate } from './validate.ts';
