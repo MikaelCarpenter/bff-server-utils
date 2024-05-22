4f4b812c6146e26e089dbf64619fec265bad907e
Mikael Carpenter <mikaelcarp.dev@gmail.com>
2024-05-21 20:55:39 -0700

    add module documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bff-server-utils
+
+Contains simple utilities I find useful when writing BFF servers.
--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,11 @@
+/**
+ * @module
+ *
+ * BFF Server Utils
+ *
+ * Contains simple utilities I find useful when writing BFF servers.
+ */
+
 export { default as prove } from './prove.ts';
 
 export { default as validate } from './validate.ts';
